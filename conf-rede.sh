9880cc9a7ce0f6346b61f0eb756395c798edaa93
AndersonSchinoff <aoschinoff@gmail.com>
2016-09-28 21:35:25 -0300

    Add files via upload

---
+++ conf-rede.sh
@@ -0,0 +1,21 @@
+# Script desenvolvido para o trabalho de g1 em redes de computadores
+#Professor Diego
+
+#!/bin/bash
+
+ip addr show
+
+echo
+echo "TECLE ENTER PARA CONTINUAR"
+read pause
+clear
+
+hostname Anderson
+echo
+echo "TECLE ENTER PARA CONTINUAR"
+read pause
+clear
+
+
+
+
---
+++ testa_ssh.sh
@@ -0,0 +1,20 @@
+# Script desenvolvido para o trabalho de g1 em redes de computadores
+#Professor Diego
+
+#!/bin/bash
+
+
+cat /etc/issue
+echo
+echo "TECLE ENTER PARA CONTINUAR"
+read pause
+clear
+
+uname -m 
+echo
+echo "TECLE ENTER PARA CONTINUAR"
+read pause
+clear 
+
+
+
